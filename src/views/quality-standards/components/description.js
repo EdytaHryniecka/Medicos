b4c2d44d3a9b9c29b70c63ae87a08db8c367f469
kplichta <konrad.plichta@sim.com.pl>
2025-05-26 13:08:11 +0200

    update/ add qs description component

--- src/views/quality-standards/components/description.js
+++ src/views/quality-standards/components/description.js
@@ -1,14 +1,28 @@
 import React from "react"
 import { useTranslation } from "gatsby-plugin-react-i18next"
 import "../styles/description.css"
-
+import { StaticImage } from "gatsby-plugin-image"
 const Description = () => {
   const { t } = useTranslation()
 
   return (
     <>
       <div className="qs-description-container">
-        <div className="container"></div>
+        <div className="container">
+          <div className="description-left-container">
+            <p className="p-style">{t`quality-standards-description.description`}</p>
+            <p className="p-style description-2">{t`quality-standards-description.description2`}</p>
+          </div>
+          <div className="image-container">
+            <StaticImage
+              className="description-image"
+              src="../../../images/quality-standards/description/qs-description.webp"
+              alt="Flag"
+              placeholder="Flag"
+              loading="lazy"
+            />
+          </div>
+        </div>
       </div>
     </>
   )
