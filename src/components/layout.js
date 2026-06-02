955ad9e2c0bd1e7829599b0162b0f9acec24e056
TasamayaAnastasiia <144161343+TasamayaAnastasiia@users.noreply.github.com>
2026-06-02 14:14:47 +0200

    feat: add tag Albacross

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Menu from "./menu/menu"
 import Footer from "./footer/footer"
 import { useLayoutEffect } from "react"
@@ -6,6 +6,20 @@ import { useLocation } from "@reach/router"
 
 const Layout = ({ children }) => {
   const location = useLocation()
+
+  // Albacross
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const script1 = document.createElement("script")
+      script1.innerHTML = 'window._nQc="89610872";'
+      document.head.appendChild(script1)
+
+      const script2 = document.createElement("script")
+      script2.async = true
+      script2.src = "https://serve.albacross.com/track.js"
+      document.head.appendChild(script2)
+    }
+  }, [])
 
   useLayoutEffect(() => {
     const timeoutId = setTimeout(() => {
